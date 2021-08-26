8cb1c3dfeb57bdc947747cabc23250e50455428d
the-theta <80995167+the-theta@users.noreply.github.com>
2021-08-26 13:56:17 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,5 +1,6 @@
 git clone -b dev https://github.com/TeamUltroid/Ultroid /root/TeamUltroid
 cp .env /root/TeamUltroid/.env
 cd /root/TeamUltroid
+export UPSTREAM_REPO=https://github.com/TeamUltroid/Ultroid
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
 docker run --privileged --env-file .env --rm -i ultroid
