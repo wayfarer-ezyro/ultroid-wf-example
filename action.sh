1022ae6fa055c53a4d2066d885e3e1d9ce201fcc
wayfarer-ezyro <72940489+wayfarer-ezyro@users.noreply.github.com>
2021-12-14 21:57:44 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,5 +1,6 @@
 git clone -b dev https://github.com/TeamUltroid/Ultroid /root/TeamUltroid
 cp .env /root/TeamUltroid/.env
 cd /root/TeamUltroid
+pip3 install cfscrape
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
 docker run --privileged --env-file .env --rm -i ultroid
